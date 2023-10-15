7ba75a3734ba3771f44b0307808a98a20b458536
annettelqw <annette.lee.2022@smu.edu.sg>
2023-10-15 21:23:56 +0800

    fixed heart function + added toggle button for auto-filtering

--- smooserve-fe/src/views/Landing.vue
+++ smooserve-fe/src/views/Landing.vue
@@ -1,17 +1,28 @@
 <script>
-import NavBar from "../components/NavBar.vue"
-import CaroPics from "../components/CaroPics.vue";
 import { onMounted, ref } from "vue";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase";
-
+import NavBar from "../components/NavBar.vue";
+import CaroPics from "../components/CaroPics.vue";
 
 export default {
-  // take info from database for CSP
   setup() {
     const csps = ref([]);
+    const isHeartRed = ref([]);
+    const selectedValue1 = ref('');
+    const selectedValue2 = ref('');
+    const selectedValue3 = ref('');
+
+    const toggleHeartColor = (index) => {
+      isHeartRed.value[index] = !isHeartRed.value[index];
+    };
+
+    const toggleAutoFilter = () => {
+      selectedValue3.value = selectedValue3.value === '' ? 'choiceX' : '';
+    };
+
     onMounted(async () => {
-      const fbCSP = []; 
+      const fbCSP = [];
       const querySnapshot = await getDocs(collection(db, "CSPs"));
 
       querySnapshot.forEach((doc) => {
@@ -25,48 +36,38 @@ export default {
       });
 
       csps.value = fbCSP;
-      console.log(fbCSP);
     });
-    return { csps };
+
+    return {
+      csps,
+      isHeartRed,
+      selectedValue1,
+      selectedValue2,
+      selectedValue3,
+      toggleHeartColor,
+      toggleAutoFilter,
+    };
   },
   components: {
     NavBar,
     CaroPics,
   },
-
-  data() {
-    return {
-      selectedValue1: '', // Initialize with default values if needed
-      selectedValue2: '',
-      selectedValue3: '',
-      isHeartRed: false, // Initially, the heart is not red
-    };
-  },
-  methods: {
-    toggleHeartColor() {
-      // Toggle the heart color between red and gray
-      this.isHeartRed = !this.isHeartRed;
-    },
-  },
 };
-
-</script> 
+</script>
 
 <template>
   <!-- navbar component -->
   <NavBar />
   <CaroPics />
 
-
   <!-- headers -->
   <div class="container-fluid" style="background-color: lightblue;">
     <div class="row pb-3">
-      <h2 style="color:black; font-weight: bold; text-align: center; margin-top: 30px; font-size: 2rem">COMMUNITY SERVICE
-        PROJECTS</h2><br>
+      <h2 style="color:black; font-weight: bold; text-align: center; margin-top: 30px; font-size: 2rem">COMMUNITY SERVICE PROJECTS</h2>
+      <br>
       <h2 style="color:red; font-weight: bold; text-align: center; font-size: 5rem;">THIS MONTH</h2>
     </div>
   </div>
-
 
   <!-- tilted cards -->
   <div class="ct">
@@ -76,7 +77,6 @@ export default {
       </div>
       <div class="card rd">
         <img style="border-radius: 15px;" src="/layout/images/card2.jpg">
-
       </div>
       <div class="card rd">
         <img style="border-radius: 15px;" src="/layout/images/card3.jpg">
@@ -84,94 +84,69 @@ export default {
     </div>
   </div>
 
-
   <!-- body -->
   <div class="container-fluid" style="background-color: navy">
     <div class="row">
       <div class="col">
         <h1 style="font-weight: bold; text-align: center; color:white;">What's happening in Smooserve</h1>
-        <div>
+      </div>
+    </div>
+    <div class="row" style="text-align: center;">
+      <!-- First Dropdown List -->
+      <div class="dropdown">
+        <label for="dropdown1">Causes:</label>
+        <select id="dropdown1" v-model="selectedValue1">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+
+        <!-- Second Dropdown List -->
+        <label for="dropdown2">Skills:</label>
+        <select id="dropdown2" v-model="selectedValue2">
+          <option value="optionA">Option A</option>
+          <option value="optionB">Option B</option>
+          <option value="optionC">Option C</option>
+        </select>
+
+        <!-- Third Dropdown List -->
+        <div class="toggle-button">
+          <label>Auto-filter:</label>
+          <button @click="toggleAutoFilter">{{ selectedValue3 === '' ? 'Off' : 'On' }}</button>
         </div>
-        <div class="row" style="text-align: center;">
-          <!-- First Dropdown List -->
-          <div class="dropdown">
-            <label for="dropdown1">Causes:</label>
-            <select id="dropdown1" v-model="selectedValue1">
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
+      </div>
 
-
-            <!-- Second Dropdown List -->
-
-            <label for="dropdown2">Skills:</label>
-            <select id="dropdown2" v-model="selectedValue2">
-              <option value="optionA">Option A</option>
-              <option value="optionB">Option B</option>
-              <option value="optionC">Option C</option>
-            </select>
-
-
-            <!-- Third Dropdown List -->
-
-            <label for="dropdown3">Date:</label>
-            <select id="dropdown3" v-model="selectedValue3">
-              <option value="choiceX">Choice X</option>
-              <option value="choiceY">Choice Y</option>
-              <option value="choiceZ">Choice Z</option>
-            </select>
-          </div>
-
-
-          <!-- Display selected values -->
-          <div class="row justify-content-center" style="color:white; font-weight: bold; font-size: 20px;">
-            Selected Value 1: {{ selectedValue1 }}
-            Selected Value 2: {{ selectedValue2 }}
-            Selected Value 3: {{ selectedValue3 }}
-          </div>
-        </div>
+      <!-- Display selected values -->
+      <div class="row justify-content-center" style="color:white; font-weight: bold; font-size: 20px;">
+        Selected Value 1: {{ selectedValue1 }}
+        Selected Value 2: {{ selectedValue2 }}
+        Selected Value 3: {{ selectedValue3 }}
       </div>
     </div>
   </div>
-
 
   <!-- font awesome library icons  -->
   <div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
   </div>
 
-  <!-- <div class="container">
-    <div class="row">
-      <div class="col">
-        <ul>
-          <li v-for="csp in csps" :key="csp.id">
-            {{ csp.title }} - {{ csp.desc }}
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div> -->
-
-  <!--cards taken from w3schools-->
   <div class="container-fluid" style="background-color: lightblue;">
     <div class="row">
-      <div class="col-md-3" v-for="csp in csps" :key="csp.id">
+      <div class="col-md-3" v-for="(csp, index) in csps" :key="csp.id">
         <div class="flip-card">
           <div class="flip-card-inner">
             <div class="flip-card-front">
               <!-- Use csp.imageURL dynamically for the image source -->
-              
-              <img :src='csp.imageURL' :alt="`CSP ${csp.id}`">
+              <img :src="csp.imageURL" :alt="`CSP ${csp.id}`">
             </div>
             <div class="flip-card-back align-items-end">
               <div class="heart-container">
-                <i class="fas fa-heart" :class="{ 'heart-red': isHeartRed }" @click="toggleHeartColor"></i>
+                <i class="fas fa-heart clickable" :class="{ 'heart-red': isHeartRed[index] }" @click="toggleHeartColor(index)"></i>
               </div>
               <div class="text-center">
                 <!-- Use csp.title and csp.desc for card content -->
                 <h1>{{ csp.title }}</h1>
-                <p>{{ csp.desc }}</p>
+                <p class="card-description">{{ csp.desc }}</p>
                 <!-- Assuming you have a route named 'csp.page', replace it with the appropriate route -->
                 <router-link :to="'/csp/' + csp.id" class="btn btn-primary m-3" role="button">See more!</router-link>
               </div>
@@ -183,8 +158,31 @@ export default {
   </div>
 </template>
 
+
 <style>
 @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css');
+
+.toggle-button button {
+  display: inline-block;
+  background-color: #007ad9; /* Set the desired background color */
+  color: white;
+  border: none;
+  border-radius: 20px;
+  padding: 5px 20px;
+  cursor: pointer;
+}
+
+.toggle-button button:hover {
+  background-color: #0053a6; /* Set the desired hover color */
+}
+
+.toggle-button button.on {
+  background-color: #007ad9; /* Set the background color when it's turned on */
+}
+
+.toggle-button button.off {
+  background-color: #ccc; /* Set the background color when it's turned off */
+}
 
 .heart-container {
   position: absolute;
@@ -202,6 +200,9 @@ export default {
   /* You can customize the red color as needed */
 }
 
+.clickable {
+  cursor: pointer;
+}
 
 /* to have spacing between each card */
 .col-md-3 {
@@ -326,7 +327,7 @@ export default {
   border-radius: 10px;
   padding: 5px 5px;
   border: 2px solid rgba(6, 66, 115);
-  ;
+;
 }
 
 .navbar-toggler {
@@ -432,6 +433,8 @@ export default {
   transform: rotateY(180deg);
   display: flex;
   justify-content: center;
+  font-size: 14px; /* Adjust the font size as needed */
+  padding: 10px; /* Add some padding for readability */
 }
 
 .flip-card img {
