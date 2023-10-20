ea71eeb3ad041ebdf5f22699cc12b56bc71b12a3
arshiya8 <jahan.arshiya1@gmail.com>
2023-10-20 22:46:00 +0800

    .

--- smooserve-fe/src/components/GoogleMaps.vue
+++ smooserve-fe/src/components/GoogleMaps.vue
@@ -2,7 +2,7 @@
     <div>
         <GoogleMap :api-key="apiKey" style="width: 100%; height: 500px" :center="{ lat: lat, lng: lng }" :zoom="15">
             <Marker :options="{ position: { lat: lat, lng: lng } }" />
-            <Marker :options="{ position: { lat: lat + 0.001, lng: lng } }" />
+            <!-- <Marker :options="{ position: { lat: lat + 0.001, lng: lng } }" /> -->
         </GoogleMap>
     </div>
 </template>
@@ -11,25 +11,25 @@
 import { GoogleMap, Marker } from 'vue3-google-map'
 
 export default {
-    methods: {
-        useGeolocation() {
-            const coords = ref({ latitude: 0, longitude: 0 })
-            const isSupported = 'navigator' in window && 'geolocation' in navigator
+    // methods: {
+    //     useGeolocation() {
+    //         const coords = ref({ latitude: 0, longitude: 0 })
+    //         const isSupported = 'navigator' in window && 'geolocation' in navigator
 
-            let watcher = null
-            onMounted(() => {
-                if (isSupported)
-                    watcher = navigator.geolocation.watchPosition(
-                        position => (coords.value = position.coords)
-                    )
-            })
-            onUnmounted(() => {
-                if (watcher) navigator.geolocation.clearWatch(watcher)
-            })
+    //         let watcher = null
+    //         onMounted(() => {
+    //             if (isSupported)
+    //                 watcher = navigator.geolocation.watchPosition(
+    //                     position => (coords.value = position.coords)
+    //                 )
+    //         })
+    //         onUnmounted(() => {
+    //             if (watcher) navigator.geolocation.clearWatch(watcher)
+    //         })
 
-            return { coords, isSupported }
-        }
-    },
+    //         return { coords, isSupported }
+    //     }
+    // },
     components: {
         GoogleMap,
         Marker
