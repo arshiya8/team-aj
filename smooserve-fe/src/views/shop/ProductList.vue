6ec407cfac17a010ae0ca28a4d71b531accf96bb
Jessa Myra Naidu <naidu.jessa@gmail.com>
2023-10-20 13:05:00 +0800

    added nav and footer, changed styles for some of the text

--- smooserve-fe/src/views/shop/ProductList.vue
+++ smooserve-fe/src/views/shop/ProductList.vue
@@ -1,11 +1,12 @@
 <script>
+// nav and footer
 import NavBar from "../../components/NavBar.vue";
+import Footer from "../../components/Footer.vue";
 
 export default {
     data() {
         return {
-            showingTooltip: false,
-            tooltipText: '',
+            
             products: [
                 {
                     id: 1,
@@ -16,8 +17,7 @@ export default {
                     CSPName: 'CSP1',
                     imageAlt: 'A cute and vibrant name tent design',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 2,
@@ -27,8 +27,7 @@ export default {
                     imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ7Pc6dUzwCP0Ec5o5AHkvaD8f99inu_ffeVgiE5uq3tb0o_vABlR9ML5izhDcYrQ5cDc&usqp=CAU",
                     imageAlt: 'A metal, insulated waterbottle',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 3,
@@ -38,8 +37,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
                     imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -49,8 +47,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -60,7 +57,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -70,8 +67,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -81,8 +77,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                
                 },
                 {
                     id: 4,
@@ -92,8 +87,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -103,8 +97,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 {
                     id: 4,
@@ -114,8 +107,7 @@ export default {
                     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
                     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
                     quantity: 0,
-                    showTooltip: false,
-                    tooltipText: '',
+                    
                 },
                 // M
                 // More products...
@@ -132,15 +124,16 @@ export default {
             }
             this.cart.push(product);
         },
-        showTooltip(product) {
-            this.showingTooltip = true;
-            this.tooltipText = product.imageAlt;
-        },
+        
     },
     computed: {
         totalCartItems() {
             return this.cart.reduce((total, product) => total + product.quantity, 0);
         },
+    },
+    components: {
+        NavBar,
+        Footer,
     },
 
     props: {
@@ -153,39 +146,43 @@ export default {
 
 <template>
     <div class="container-fluid">
+        <NavBar />
         <!-- Image with overlay -->
-        <div class="bg-img">
+        <div class="bg-img relative">
             <img src="https://orientation.smu.edu.sg/sites/vivace.smu.edu.sg/files/styles/coverphoto/public/vivace/coverphotos/Verts%20Banner.jpeg?itok=HWSA3IuM"
                 alt="Jumbotron Image" class="w-full h-auto" />
-        </div>
-        <div class="bg-text">
-            <h1 class="text-5xl font-serif mb-4">Smooserve Shop</h1>
-            <p class="text-lg font-italic">Support your favorite CSP's today by purchasing merchandise</p>
+            <div
+                class="bg-text flex flex-column justify-center items-center text-center absolute top-0 left-0 right-0 bottom-0">
+                <h1 class="text-5xl mb-4 font-bold text-white"
+                    style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">Smooserve Shop</h1>
+                <p class="text-lg font-italic text-white"
+                    style="position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%);">Support your favorite
+                    CSP's today by purchasing merchandise</p>
+            </div>
         </div>
     </div>
-
     <div class="container">
         <div class="row justify-content-center">
-                <div class="col-md-3 mb-4" v-for="product in products" :key="product.id" >
-                    <div class="p-card">
-                        <div class="overflow-hidden rounded-lg bg-gray-200 product-image"
-                            @mouseenter="showTooltip(product)">
-                            <img :src="product.imageSrc" :alt="product.imageAlt" class="card-image" />
-                        </div>
-                        <div class="p-card-title mt-4 text-sm text-gray-700">{{ product.name }}</div>
-                        <div class="p-card-subtitle mt-1 text-lg font-medium text-gray-900">{{ product.price }}</div>
-                        <div class="p-card-body">
-                            <button @click="addToCart(product)" class="p-button p-button-info p-button-raised">
-                                Add to Cart
-                            </button>
-                        </div>
-                        <div class="tooltip" v-if="showingTooltip">
-                            <p class="text-lg text-gray-300">{{ tooltipText }}</p>
-                        </div>
+            <div class="col-md-3 mb-4" v-for="product in products" :key="product.id">
+                <div class="p-card">
+                    <div class="overflow-hidden rounded-lg bg-gray-200 product-image" @mouseenter="showTooltip(product)">
+                        <img :src="product.imageSrc" :alt="product.imageAlt" class="card-image" />
+                    </div>
+                    <div class="p-card-title mt-4 text-sm text-gray-700">{{ product.name }}</div>
+                    <div class="p-card-subtitle mt-1 text-lg font-medium text-gray-900">{{ product.price }}</div>
+                    <div class="p-card-body">
+                        <button @click="addToCart(product)" class="p-button p-button-info p-button-raised">
+                            Add to Cart
+                        </button>
+                    </div>
+                    <div class="tooltip" v-if="showingTooltip">
+                        <p class="text-lg text-gray-300">{{ tooltipText }}</p>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
+    <Footer />
 </template>
   
 
@@ -196,7 +193,6 @@ export default {
 .bg-img {
     position: relative;
     display: flex;
-
 }
 
 .bg-text {
@@ -209,17 +205,24 @@ export default {
 
 .p-card {
     width: 300px;
+    height: 350px;
+    /* Increase the card height to accommodate larger images */
     border: 1px solid #ccc;
     border-radius: 5px;
     overflow: hidden;
     margin: 16px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
 }
 
 .card-image {
     width: 100%;
+    /* Set the image width to 100% of its container */
     height: 200px;
-    object-fit: cover;
+    /* Set the image height as per your preference */
+    object-fit: contain;
+    /* Keep the image centered and fit the box */
 }
 
 .p-card-title {
@@ -237,5 +240,22 @@ export default {
 .p-card-body {
     margin: 0 12px 12px 12px;
 }
-</style>
-  
+
+/* Style for the "Add to Cart" button */
+.p-button {
+    background-color: transparent;
+    /* Remove the background color */
+    border: none;
+    /* Remove the button border */
+    color: #3490dc;
+    /* Set the text color (you can choose your desired color) */
+    cursor: pointer;
+    font-size: 1rem;
+    margin: 0;
+    padding: 0;
+}
+
+.p-button:hover {
+    text-decoration: underline;
+    /* Add underline on hover (optional) */
+}</style>
