408d79f2ad8a5a2181b07b33cd4155fb45a07041
annettelqw <annette.lee.2022@smu.edu.sg>
2023-10-28 21:34:41 +0800

    updated navbar

--- smooserve-fe/src/components/NavBar.vue
+++ smooserve-fe/src/components/NavBar.vue
@@ -69,71 +69,52 @@ const items = ref([
     icon: "pi pi-shopping-cart",
     routeName: "Shop",
   },
-  
+
 ]);
 </script>
 
 <template>
   <div class="grid align-items-center justify-content-center">
     <div class="col-12 md:col-12 lg:col-10">
-      <Menubar
-        :model="items"
-        :pt="{
-          action: ({ props, state, context }) => ({
-            class: context.active
-              ? 'bg-primary-50 border-round-sm'
-              : context.focused
+      <Menubar :model="items" :pt="{
+        action: ({ props, state, context }) => ({
+          class: context.active
+            ? 'bg-primary-50 border-round-sm'
+            : context.focused
               ? 'bg-primary-100 border-round-sm'
               : undefined,
-          }),
-        }"
-        class="mb-3 shadow-none surface-ground border-none"
-      >
+        }),
+      }" class="mb-3  surface-0 border-none">
         <template #start>
           <img alt="logo" :src="logoUrl" height="60" class="mr-2" />
         </template>
-        <template
-          #item="{ label, item, props, root, hasSubmenu }"
-          class="p-menuitem-active"
-        >
-          <router-link
-            v-if="item.routeName"
-            v-slot="routerProps"
-            :to="{ name: item.routeName }"
-            custom
-            :exact="true"
-          >
+        <template #item="{ label, item, props, root, hasSubmenu }" class="p-menuitem-active">
+          <router-link v-if="item.routeName" v-slot="routerProps" :to="{ name: item.routeName }" custom :exact="true">
             <a :href="routerProps.href" v-bind="props.action">
               <span v-bind="props.icon" />
               <span v-bind="props.label">{{ label }}</span>
             </a>
           </router-link>
-          <a
-            v-else
-            :href="item.url"
-            :target="item.target"
-            v-bind="props.action"
-          >
+          <a v-else :href="item.url" :target="item.target" v-bind="props.action">
             <span v-bind="props.icon" />
             <span v-bind="props.label">{{ label }}</span>
-            <span
-              :class="[
-                hasSubmenu &&
-                  (root ? 'pi pi-fw pi-angle-down' : 'pi pi-fw pi-angle-right'),
-              ]"
-              v-bind="props.submenuicon"
-            />
+            <span :class="[
+              hasSubmenu &&
+              (root ? 'pi pi-fw pi-angle-down' : 'pi pi-fw pi-angle-right'),
+            ]" v-bind="props.submenuicon" />
           </a>
         </template>
         <template #end>
-              <a class='line-remove' v-if="isLoggedIn" @click="handleSignOut">
-                <router-link :to="{ name: 'Home' }"><i class="pi pi-sign-out icon-spacing pr-2"></i>
-                <span style="text-decoration: none;">LOG OUT</span></router-link>
-                <router-link :to="{ name: 'Profile' }"><i class="pi pi-user px-4" style="font-size: 1.2rem"></i></router-link>
-              </a>
+          <a class='line-remove' v-if="isLoggedIn" @click="handleSignOut">
+            <router-link :to="{ name: 'Home' }"><i class="pi pi-sign-out icon-spacing pr-2"></i>
+              <span style="text-decoration: none;">LOG OUT</span></router-link>
+            </a>
+              <router-link v-if="isLoggedIn" :to="{ name: 'Profile' }"><i class="pi pi-user px-4" style="font-size: 1.2rem"></i></router-link>
           <a class='line-remove' v-else>
-              <i class="navbar-icon pi pi-sign-in px-2"></i><router-link :to="{ name: 'Login' }"><span class="pr-4">LOG IN</span></router-link>
-              <i class="navbar-icon pi pi-user-plus px-2"></i><router-link :to="{ name: 'Register' }"><span >SIGN IN</span></router-link>
+            <i class="navbar-icon pi pi-sign-in px-2"></i><router-link :to="{ name: 'Login' }"><span class="pr-4">LOG
+                IN</span></router-link>
+            <i class="navbar-icon pi pi-user-plus px-2"></i><router-link :to="{ name: 'Register' }"><span>SIGN
+                IN</span></router-link>
           </a>
         </template>
       </Menubar>
@@ -141,7 +122,7 @@ const items = ref([
   </div>
 </template>
 <style>
-a{
+a {
   text-decoration: none !important;
   color: black !important;
 }
