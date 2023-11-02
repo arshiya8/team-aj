356683dc6509397df8fd5d21b0c7bd74da76f9a8
annettelqw <annette.lee.2022@smu.edu.sg>
2023-11-02 20:54:30 +0800

    added quiz validation functions, and also edited some quiz questions

--- smooserve-fe/src/views/quiz/Quiz.vue
+++ smooserve-fe/src/views/quiz/Quiz.vue
@@ -11,11 +11,13 @@ export default {
       },
       submitted: false, // Add this property to track form submission
       showHomepageButton: false, // Add this property to control button visibility
+      questionsAnswered: new Set(),
+      allQuestionsAnswered: false,
 
       questions: [
         {
           name: "first_name",
-          text: "1. What is your full name?",
+          text: "1. What is your first name?",
           type: "open-ended",
           background: "../../layout/images/quiz1.png",
           character_limit: 100,
@@ -31,7 +33,7 @@ export default {
         },
         {
           name: "telegram",
-          text: "3. What is your telegram handle? Eg. @donald",
+          text: "3. What is your telegram handle?",
           type: "open-ended",
           background: "../../layout/images/quiz3.png",
           character_limit: 100,
@@ -120,16 +122,16 @@ export default {
           },
         },
         {
-          name: "self_description",
-          text: "10. Describe yourself in 100 words.",
+          name: "strengths",
+          text: "10. What are your strengths?",
           type: "open-ended",
           background: "../../layout/images/quiz10.png",
           character_limit: 1000,
           response: ''
         },
         {
-          name: "self_awareness",
-          text: "11. What are your strengths and weaknesses?",
+          name: "weaknesses",
+          text: "11. What are your weaknesses?",
           type: "open-ended",
           background: "../../layout/images/quiz11.png",
           character_limit: 1000,
@@ -145,18 +147,22 @@ export default {
         this.updateBackground();
       }
     },
+
     nextQuestion() {
       if (this.currentQuestion < this.questions.length - 1) {
         this.currentQuestion++;
         this.updateBackground();
       }
     },
-    updateBackground() {  // Update background of webpage
+
+    updateBackground() {
       this.backgroundImage.backgroundImage = 'url(' + this.questions[this.currentQuestion].background + ')';
     },
+
     goToQuestion(questionIndex) {
       this.currentQuestion = questionIndex;
     },
+
     updateMultiSelect(groupName, value) {
       if (!this.selectedOptions.hasOwnProperty(groupName)) {
         this.selectedOptions[groupName] = [];
@@ -170,33 +176,73 @@ export default {
         this.selectedOptions[groupName].splice(index, 1);
       }
 
+      // Add a log statement to see which question was answered
+      console.log("Answered question:", groupName);
+
+      // Update the questionsAnswered set
+      this.questionsAnswered.add(groupName);
     },
+
     updateResponse(groupName, value, questionType) {
-      // Change to enum if got time
-      if (questionType === "multi-select") {  // Process questions where type is multi-select
+      if (questionType === "multi-select") {
         this.updateMultiSelect(groupName, value);
       } else if (questionType === "single-select") {
-        // Process questions where type is single-select
         this.selectedOptions[groupName] = value;
-      } else {  // Process questions where type is open-ended (value here is question index)
+      } else {
         this.selectedOptions[groupName] = this.questions[value].response;
       }
 
+      // Add a log statement to see which question was answered
+      console.log("Answered question:", this.questions[this.currentQuestion].name);
+
+      // Update the questionsAnswered set
+      this.questionsAnswered.add(groupName);
     },
+
+    isQuestionAnswered(questionName) {
+      return this.questionsAnswered.has(questionName);
+    },
+
     submitForm() {
-      // Handle form submission here
-      console.log("Form submitted with selected options:");
+      // Check if all questions have been answered
+      this.allQuestionsAnswered = this.questions.every(question => this.questionsAnswered.has(question.name));
 
-      Object.entries(this.selectedOptions).forEach(([k, v]) => {
-        console.log("The key: ", k);
-        console.log("The value: ", v);
-      });
+      if (this.allQuestionsAnswered) {
+        console.log("Form submitted with selected options:");
+        Object.entries(this.selectedOptions).forEach(([k, v]) => {
+          console.log("The key: ", k);
+          console.log("The value: ", v);
+        });
 
-      // Set submitted to true and show the button
-      this.submitted = true;
-      this.showHomepageButton = true;
+        this.submitted = true;
+        this.showHomepageButton = true;
+      }
+    },
+
+  },
+
+  computed: {
+
+    isSubmitButtonVisible() {
+      return this.allQuestionsAnswered && this.currentQuestion === this.questions.length - 1;
+    },
+
+    isNextButtonDisabled() {
+      const currentQuestion = this.questions[this.currentQuestion];
+
+      if (currentQuestion.type === "open-ended") {
+        return !currentQuestion.response.trim();
+      } else if (currentQuestion.type === "single-select") {
+        return !this.selectedOptions[currentQuestion.name];
+      } else if (currentQuestion.type === "multi-select") {
+        return !this.selectedOptions[currentQuestion.name] || this.selectedOptions[currentQuestion.name].length === 0;
+      }
+
+      return false; // Enable the "Next" button by default
     },
   },
+
+
 };
 </script>
 
@@ -207,7 +253,7 @@ export default {
 
     <!-- Smooserve logo -->
     <a href="#" style="display: flex; justify-content: center; align-items: center;">
-      <img src="layout/images/logo-white.png" height="80" class="mr-0 lg:mr-2" />
+      <img src="/layout/images/logo-white.png" height="80" class="mr-0 lg:mr-2" />
     </a>
 
     <!-- Container for header -->
@@ -225,24 +271,19 @@ export default {
           <!-- Display question options or text area -->
           <div>
             <table v-if="questions[currentQuestion].type === 'multi-select'" class="w-full">
-              <tr
-                  class="wrapping custom-radio-label"
-                  @click="updateMultiSelect(questions[currentQuestion].name, option)"
+              <tr class="wrapping custom-radio-label" @click="updateMultiSelect(questions[currentQuestion].name, option)"
                   :class="{
-                    selected:
-                      questions[currentQuestion].name in selectedOptions &&
-                      selectedOptions[questions[currentQuestion].name].indexOf(option) >= 0,
-                  }"
-                  v-for="(option, optionIndex) in questions[currentQuestion].options.cols"
-                  style="display: flex; flex-direction: row;"
-              >
+                  selected:
+                    questions[currentQuestion].name in selectedOptions &&
+                    selectedOptions[questions[currentQuestion].name].indexOf(option) >= 0,
+                }" v-for="(option, optionIndex) in questions[currentQuestion].options.cols"
+                  style="display: flex; flex-direction: row;">
                 <td :key="optionIndex" class="w-100 row-option text-align-start" :style="{
-                    backgroundColor: questions[currentQuestion].name in selectedOptions &&
-                      selectedOptions[questions[currentQuestion].name].indexOf(option) >= 0
-                      ? 'rgb(107, 161, 237)' // Highlight background color for selected option
-                      : 'transparent' // Background color for unselected options
-                  }"
-                >
+                  backgroundColor: questions[currentQuestion].name in selectedOptions &&
+                    selectedOptions[questions[currentQuestion].name].indexOf(option) >= 0
+                    ? 'rgb(107, 161, 237)' // Highlight background color for selected option
+                    : 'transparent' // Background color for unselected options
+                }">
                   {{ option }}</td>
                 <td class="row-checkbox text-align-end">
                   {{
@@ -256,20 +297,16 @@ export default {
             </table>
 
             <table v-else-if="questions[currentQuestion].type === 'single-select'" class="w-full">
-              <tr
-                  class="wrapping custom-radio-label"
-                  @click="updateResponse(questions[currentQuestion].name, option, questions[currentQuestion].type)"
-                  :class="{
-                    selected: selectedOptions[questions[currentQuestion].name] === option,
-                  }"
-                  v-for="(option, optionIndex) in questions[currentQuestion].options.cols"
-                  style="display: flex; flex-direction: row; align-items: center; cursor: pointer;"
-              >
+              <tr class="wrapping custom-radio-label"
+                  @click="updateResponse(questions[currentQuestion].name, option, questions[currentQuestion].type)" :class="{
+                  selected: selectedOptions[questions[currentQuestion].name] === option,
+                }" v-for="(option, optionIndex) in questions[currentQuestion].options.cols"
+                  style="display: flex; flex-direction: row; align-items: center; cursor: pointer;">
                 <td :key="optionIndex" class="w-full row-option text-align-start" :style="{
-                    backgroundColor: selectedOptions[questions[currentQuestion].name] === option
-                      ? 'rgb(107, 161, 237)' // Highlight background color for selected option
-                      : 'transparent' // Background color for unselected options
-                  }">{{ option }}</td>
+                  backgroundColor: selectedOptions[questions[currentQuestion].name] === option
+                    ? 'rgb(107, 161, 237)' // Highlight background color for selected option
+                    : 'transparent' // Background color for unselected options
+                }">{{ option }}</td>
                 <td class="row-checkbox text-align-end">
                   {{ selectedOptions[questions[currentQuestion].name] === option ? '✓' : '' }}
                 </td>
@@ -278,14 +315,10 @@ export default {
 
             <div v-else style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
               <span class="p-float-label w-full mt-3 mx-2">
-                <Textarea
-                    v-model="questions[currentQuestion].response"
-                    :rows="10"
-                    :maxlength="questions[currentQuestion].character_limit"
-                    class="w-full"
-                    style="resize: none; width: auto;"
-                    @mouseout="updateResponse(questions[currentQuestion].name, currentQuestion, questions[currentQuestion].type)"
-                />
+                <Textarea v-model="questions[currentQuestion].response" :rows="10"
+                          :maxlength="questions[currentQuestion].character_limit" class="w-full"
+                          style="resize: none; width: auto;"
+                          @mouseout="updateResponse(questions[currentQuestion].name, currentQuestion, questions[currentQuestion].type)" />
                 <label>Enter response here</label>
               </span>
             </div>
@@ -293,15 +326,13 @@ export default {
 
           <!-- Back and Next buttons for each question -->
           <div class="d-flex justify-content-between my-3 mx-2">
-            <Button v-if="currentQuestion > 0" type="button" label="Back" @click="prevQuestion" class="mr-2">
-            </Button>
-            <div v-else></div>
-            <Button v-if="currentQuestion < questions.length - 1" type="button" label="Next" @click="nextQuestion" class="ml-2">
-            </Button>
-            <Button v-if="currentQuestion === questions.length - 1" type="submit" label="Submit">
-            </Button>
+            <Button v-if="currentQuestion > 0" type="button" label="Back" @click="prevQuestion" class="mr-2"></Button>
+            <div v-else></div> <!-- Render nothing in place of "Back" button on the first question -->
+            <Button v-if="currentQuestion < questions.length - 1" type="button" label="Next" @click="nextQuestion"
+                    class="ml-2" :disabled="isNextButtonDisabled"></Button>
+            <Button v-if="currentQuestion === questions.length - 1" type="submit" label="Submit"></Button>
+            <!-- Render "Submit" button only on the last question -->
           </div>
-
 
           <!-- "Take Me to Homepage" button -->
           <router-link v-if="showHomepageButton" :to="{ name: 'Home' }">
@@ -328,7 +359,8 @@ export default {
   text-align: center;
   color: black;
   font-size: 25px;
-  background-color: rgba(255, 255, 255, 0.7); /* Add a semi-transparent background color */
+  background-color: rgba(255, 255, 255, 0.7);
+  /* Add a semi-transparent background color */
 }
 
 body {
@@ -401,5 +433,4 @@ body {
   /* Dark purple for selected option */
   color: #ffffff;
 }
-
 </style>
