205f36078a8657a0bdc8a2da414c4ef38fd17b19
jontoh <75199733+jontohh@users.noreply.github.com>
2023-11-04 14:17:40 +0800

    fix for vercel

--- smooserve-fe/src/views/maps/Map.vue
+++ smooserve-fe/src/views/maps/Map.vue
@@ -1,7 +1,7 @@
 <script>
-import Navbar from "../../components/Navbar.vue";
-import GoogleMaps from "../../components/GoogleMaps.vue";
-import Footer from "../../components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
+import GoogleMaps from "@/components/GoogleMaps.vue";
+import Footer from "@/components/Footer.vue";
 
 export default{
     components: {
