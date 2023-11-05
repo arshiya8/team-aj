9ca127ac0d6dc171c415e2304df9d1e6801dc32c
jontoh <75199733+jontohh@users.noreply.github.com>
2023-11-06 01:16:02 +0800

    hash fixes

--- smooserve-fe/src/router/index.js
+++ smooserve-fe/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 //add your page route here
@@ -149,7 +149,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
